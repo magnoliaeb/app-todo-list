52fc6d04ce3e5fa1d51fa562b1a31a4ddd255559
Breimer Correa <breimerct@gmail.com>
2020-11-17 22:43:34 -0500

    limpieza del proyecto

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <v-app-bar app color="primary" dark >
+      <v-btn text>
+        <span class="mr-2">To Do APP</span>
+      </v-btn>
+    </v-app-bar>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <section class="home mt-5 ">
+    <div class="text-center">
+      <img src="../assets/maleTask.svg" width="500">
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
+  components: {},
 });
 </script>
